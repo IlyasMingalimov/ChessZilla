86672742165e3eb91b32876fd0fa2bf7f34559d2
neugolok@outlook.com <neugolok@outlook.com>
2024-08-26 17:30:12 +0400

    Add project files.

---
+++ ChessZilla.Core/Engine.fs
@@ -0,0 +1,38 @@
+﻿namespace ChessZilla.Core
+
+type Color = White | Black
+
+type Piece =
+    | King of Color
+    | Queen of Color
+    | Rook of Color
+    | Bishop of Color
+    | Knight of Color
+    | Pawn of Color
+
+type Position = 
+    {
+        color : Color
+        pieces : Option<Piece> array2d
+        evalation : float
+    }
+
+type CalculationTree =
+    | Root of CalculationTree 
+    | Leaf of Position
+
+module Engine =
+
+    let nextPositions (position : Position) (deep : int) : Position seq = seq{
+        yield position
+    }
+
+    let Eval (position : Position) =
+        position
+
+    let calculation (position : Position) (deep : int) =
+        position
+        |> nextPositions <| deep
+        |> Seq.map (fun p -> p |> Eval)
+        |> Seq.maxBy (fun p -> p.evalation)
+        
