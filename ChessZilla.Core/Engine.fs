52d5662c51fdaec15676c3fd0e39d4b3fe8cdc9b
IlyasMingalimov <neugolok@gmail.com>
2024-10-01 15:50:04 +0400

    Rework

--- ChessZilla.Core/Engine.fs
+++ ChessZilla.Core/Engine.fs
@@ -12,27 +12,48 @@ type Piece =
 
 type Position = 
     {
-        color : Color
-        pieces : Option<Piece> array2d
-        evalation : float
+        evaluation: float
+        ``current move`` : Color
+        preview : Position option
     }
-
-type CalculationTree =
-    | Root of CalculationTree 
-    | Leaf of Position
 
 module Engine =
 
-    let nextPositions (position : Position) (deep : int) : Position seq = seq{
-        yield position
+    let ``set evaluation`` position =
+        position
+
+    let ``get next positions`` position = seq{
+        yield ``set evaluation`` position
     }
 
-    let Eval (position : Position) =
-        position
+    let rec ``get all move options`` position deep acc = seq{
+        let nexts = ``get next positions`` position
+        if deep >= acc then
+            yield! nexts
+        else
+            for next in nexts do
+                if next.evaluation = 100.0 then
+                    yield next
+                else if next.evaluation = -100.0 then
+                    yield next
+                else 
+                    yield! ``get all move options`` next deep (acc + 1u)
 
-    let calculation (position : Position) (deep : int) =
-        position
-        |> nextPositions <| deep
-        |> Seq.map (fun p -> p |> Eval)
-        |> Seq.maxBy (fun p -> p.evalation)
+    }
+
+    let rec ``get first move`` position = 
+        match position.preview with
+            | Some(position) -> ``get first move`` position
+            | None -> position
+
+    let ``best move`` position deep =
+        let options = ``get all move options`` position  deep 0u
+        let ``best end`` =
+            match position.``current move`` with
+                | White -> 
+                    options |> Seq.maxBy (fun p -> p.evaluation)
+                | Black -> 
+                    options |> Seq.minBy (fun p -> p.evaluation)
+
+        ``get first move`` ``best end``
         
