d25353e7e68631e8c14179b1e9cdce1b0db8b55a
IlyasMingalimov <neugolok@gmail.com>
2024-10-21 17:11:16 +0400

    Add Fen Support

--- ChessZilla.Core/Engine.fs
+++ ChessZilla.Core/Engine.fs
@@ -12,48 +12,63 @@ type Piece =
 
 type Position = 
     {
-        evaluation: float
         ``current move`` : Color
-        preview : Position option
+        Pieces : Piece option seq
     }
 
 module Engine =
-
-    let ``set evaluation`` position =
-        position
-
-    let ``get next positions`` position = seq{
-        yield ``set evaluation`` position
-    }
-
-    let rec ``get all move options`` position deep acc = seq{
-        let nexts = ``get next positions`` position
-        if deep >= acc then
-            yield! nexts
-        else
-            for next in nexts do
-                if next.evaluation = 100.0 then
-                    yield next
-                else if next.evaluation = -100.0 then
-                    yield next
-                else 
-                    yield! ``get all move options`` next deep (acc + 1u)
-
-    }
-
-    let rec ``get first move`` position = 
-        match position.preview with
-            | Some(position) -> ``get first move`` position
-            | None -> position
-
-    let ``best move`` position deep =
-        let options = ``get all move options`` position  deep 0u
-        let ``best end`` =
-            match position.``current move`` with
-                | White -> 
-                    options |> Seq.maxBy (fun p -> p.evaluation)
-                | Black -> 
-                    options |> Seq.minBy (fun p -> p.evaluation)
-
-        ``get first move`` ``best end``
         
+        let next_positions postion = seq{
+            yield postion
+        }
+
+        let eval_position position =
+            position, 1.0
+
+        let best_move start_position deep =
+
+            let accum : Position seq = list.Empty
+
+            for move_count in [1..deep] do
+                let nexts = next_positions start_position
+                let eval_nexts = nexts |> Seq.map (fun x -> eval_position x)
+                let max = eval_nexts |> Seq.maxBy (fun (_,eval) -> eval)
+                printfn "%O" max
+            start_position
+
+
+module Fen =
+    let string_piece_to_type (pieces: string) = seq{
+        for line in pieces.Split [|'/'|] do
+            match System.Int32.TryParse line with
+            | true, int -> yield! seq { for i in 1..int -> None }
+            | _ ->
+                for piece in line do
+                    match piece with
+                        | 'p' -> yield Some(Pawn(Black))
+                        | 'n' -> yield Some(Knight(Black))
+                        | 'b' -> yield Some(Bishop(Black))
+                        | 'r' -> yield Some(Rook(Black))
+                        | 'q' -> yield Some(Queen(Black))
+                        | 'k' -> yield Some(King(Black))
+                        | 'P' -> yield Some(Pawn(White))
+                        | 'N' -> yield Some(Knight(White))
+                        | 'B' -> yield Some(Bishop(White))
+                        | 'R' -> yield Some(Rook(White))
+                        | 'Q' -> yield Some(Queen(White))
+                        | 'K' -> yield Some(King(White))
+                        | _ -> yield None
+    }
+
+    let string_to_position (str: string) =
+        let separete = str.Split [|' '|]
+        let pieces = string_piece_to_type separete[0]
+        let move = match separete[1] with |"w" -> White |_ -> Black     
+        {
+            ``current move`` = move
+            Pieces = pieces
+        }
+
+    let position_to_string (position : Position) =
+        let mutable result = ""
+        ""
