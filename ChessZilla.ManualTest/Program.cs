93aa7abb3919066e7961fe60affb58d6f4ee5fa3
IlyasMingalimov <neugolok@gmail.com>
2024-10-22 11:06:56 +0400

    .

--- ChessZilla.ManualTest/Program.cs
+++ ChessZilla.ManualTest/Program.cs
@@ -1,7 +1,14 @@
-﻿internal class Program
+﻿using ChessZilla.Core;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var position = Fen.ToPosition("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        var engine = new Engine();
+        foreach(var next in engine.Calculation(position, 3, 3)) 
+        { 
+            Console.WriteLine(next);
+        }
     }
 }
